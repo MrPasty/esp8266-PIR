03e24332aced90670bf7fdce442143dd084a9e64
Mr Pasty <31127617+MrPasty@users.noreply.github.com>
2017-08-18 10:30:59 +0100

    Create ConnectWiFi.py
    Handle wifi connections on ESP8266

---
+++ ConnectWiFi.py
@@ -0,0 +1,21 @@
+def connect():
+    import network
+ 
+    ssid = "YOUR SSID"
+    password =  "YOUR PASSWORD"
+ 
+    station = network.WLAN(network.STA_IF)
+ 
+    if station.isconnected() == True:
+        print("Already connected")
+        return
+ 
+    station.active(True)
+    station.connect(ssid, password)
+ 
+    while station.isconnected() == False:
+        pass
+ 
+    print("Connection successful")
+    print(station.ifconfig())
+
